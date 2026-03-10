8e87cfbcdf11e3cae3f3a0c0bd1328d8757494a6
kannn6 <kauanhg0606@gmail.com>
2026-03-10 20:51:14 -0300

    Começo da calculadora de resistores
    Base para fazer a calculadora de resistores

---
+++ calculadoraDeResistores.js
@@ -0,0 +1,11 @@
+let faixas = parseInt(prompt("Digite a quantidade de faixas do resistor, 4 ou 5."))
+let cor1 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata: "))
+let cor2 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata: "))
+let cor3 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata: "))
+let cor4= parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata:"))
+if(faixas == 5){
+let cor5 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata:"))
+}
+if(cor == 0){
+    
+}
