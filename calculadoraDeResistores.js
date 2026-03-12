819531390a2a446631400718bfc952f7ff9f5cbf
kannn6 <kauanhg0606@gmail.com>
2026-03-12 20:40:09 -0300

    reinicio usando vetores
    Começando novamente agora usando vetores

--- calculadoraDeResistores.js
+++ calculadoraDeResistores.js
@@ -1,11 +1,16 @@
-let faixas = parseInt(prompt("Digite a quantidade de faixas do resistor, 4 ou 5."))
-let cor1 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata: "))
-let cor2 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata: "))
-let cor3 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata: "))
-let cor4= parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata:"))
-if(faixas == 5){
-let cor5 = parseInt(prompt("Digite a cor da primeira faixa,\n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata:"))
+let resultado
+let valorBase
+let faixas = parseInt(prompt("Digite a quantidade de faixas do resistor, 4 ou 5."));
+let coresEscolhidas = [];
+for(i = 0;i < faixas;i++){
+let cor = parseInt(prompt("Digite a cor das faixas em ordem, \n 0-Preto\n1-Marrom\n2-Vermelho\n3-Laranja\n4-Amarelo\n5-Verde\n6-Azul\n7-Violeta\n8-Cinza\n9-Branco\n10-Ouro\n11-Prata:"));
+coresEscolhidas.push(cor);
 }
-if(cor == 0){
-    
+if(faixas == 4){
+ valorBase = (coresEscolhidas[0] * 10) + coresEscolhidas[1];
+ resultado = valorBase * Math.pow(10 , coresEscolhidas[2]);
+}else{
+ valorBase = (coresEscolhidas[0] * 100)+(coresEscolhidas[1] * 10) + coresEscolhidas[2];
+ resultado = valorBase * Math.pow(10 , coresEscolhidas[3]);
 }
+
