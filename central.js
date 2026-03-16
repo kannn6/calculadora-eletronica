af5c72fee7891a54241a294d1419b740ffae06c1
kannn6 <kauanhg0606@gmail.com>
2026-03-16 20:09:23 -0300

    Função finalizada
    Função finalizada, código completo

--- central.js
+++ central.js
@@ -1,5 +1,13 @@
-let escolha = parseInt(prompt("Digite 1-Calculadora de Consumo\n2-Calculadora de Resistores\n3-Calculadora Mul&Sub"))
+let escolha = parseInt(prompt("Digite conforme: \n 1-Calculadora de Consumo\n2-Calculadora de Resistores\n3-Calculadora Mul&Sub"))
 
+switch(escolha){
 case 1:
     calculadoraDeConsumo()
     break
+case 2:
+    calculadoraDeResistores()
+    break
+case 3:
+    calculadoraMultSub()
+    break
+}
